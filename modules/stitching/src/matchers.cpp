b5387082c0ef0adf9d403423e82168c60d5b55f8
Jiri Horner <laeqten@gmail.com>
2016-07-06 11:22:37 +0200

    affine matcher: remove duplicite code
    rework flow to get rid of duplicite code

--- modules/stitching/src/matchers.cpp
+++ modules/stitching/src/matchers.cpp
@@ -811,13 +811,6 @@ void AffineBestOf2NearestMatcher::match(const cv::detail::ImageFeatures &feature
         return;
     }
 
-    // extend H to represent linear tranformation in homogeneous coordinates
-    matches_info.H.push_back(cv::Mat::zeros(1, 3, CV_64F));
-    matches_info.H.at<double>(2, 2) = 1;
-
-    /* TODO: should we handle determinant ~ 0 (can it happen due to agressive
-    * scaling?) */
-
     // Find number of inliers
     matches_info.num_inliers = 0;
     for (size_t i = 0; i < matches_info.inliers_mask.size(); ++i)
@@ -833,38 +826,38 @@ void AffineBestOf2NearestMatcher::match(const cv::detail::ImageFeatures &feature
     // matches_info.confidence = matches_info.confidence > 3. ? 0. : matches_info.confidence;
 
     // Check if we should try to refine motion
-    if (matches_info.num_inliers < num_matches_thresh2_)
-        return;
-
-    // Construct point-point correspondences for inliers only
-    src_points.create(1, matches_info.num_inliers, CV_32FC2);
-    dst_points.create(1, matches_info.num_inliers, CV_32FC2);
-    int inlier_idx = 0;
-    for (size_t i = 0; i < matches_info.matches.size(); ++i)
+    if (matches_info.num_inliers >= num_matches_thresh2_)
     {
-        if (!matches_info.inliers_mask[i])
-            continue;
+        // Construct point-point correspondences for inliers only
+        src_points.create(1, matches_info.num_inliers, CV_32FC2);
+        dst_points.create(1, matches_info.num_inliers, CV_32FC2);
+        int inlier_idx = 0;
+        for (size_t i = 0; i < matches_info.matches.size(); ++i)
+        {
+            if (!matches_info.inliers_mask[i])
+                continue;
 
-        const DMatch& m = matches_info.matches[i];
+            const DMatch& m = matches_info.matches[i];
 
-        Point2f p = features1.keypoints[m.queryIdx].pt;
-        p.x -= features1.img_size.width * 0.5f;
-        p.y -= features1.img_size.height * 0.5f;
-        src_points.at<Point2f>(0, inlier_idx) = p;
+            Point2f p = features1.keypoints[m.queryIdx].pt;
+            p.x -= features1.img_size.width * 0.5f;
+            p.y -= features1.img_size.height * 0.5f;
+            src_points.at<Point2f>(0, inlier_idx) = p;
 
-        p = features2.keypoints[m.trainIdx].pt;
-        p.x -= features2.img_size.width * 0.5f;
-        p.y -= features2.img_size.height * 0.5f;
-        dst_points.at<Point2f>(0, inlier_idx) = p;
+            p = features2.keypoints[m.trainIdx].pt;
+            p.x -= features2.img_size.width * 0.5f;
+            p.y -= features2.img_size.height * 0.5f;
+            dst_points.at<Point2f>(0, inlier_idx) = p;
 
-        inlier_idx++;
+            inlier_idx++;
+        }
+
+        // Rerun motion estimation on inliers only
+        if (full_affine_)
+            estimateAffine2D(src_points, dst_points, matches_info.H, matches_info.inliers_mask);
+        else
+            estimateAffinePartial2D(src_points, dst_points, matches_info.H, matches_info.inliers_mask);
     }
-
-    // Rerun motion estimation on inliers only
-    if (full_affine_)
-        estimateAffine2D(src_points, dst_points, matches_info.H, matches_info.inliers_mask);
-    else
-        estimateAffinePartial2D(src_points, dst_points, matches_info.H, matches_info.inliers_mask);
 
     // extend H to represent linear tranformation in homogeneous coordinates
     matches_info.H.push_back(cv::Mat::zeros(1, 3, CV_64F));
